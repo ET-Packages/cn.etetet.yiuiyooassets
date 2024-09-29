628cb9f47170bc09353d9a520eb8ed20f68eb419
Lsy <379338943@qq.com>
2024-09-29 15:40:12 +0800

    扩展

---
+++ Scripts/HotfixView/Client/YIUIFactory_YooAsset.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ET.Client
+{
+    public static partial class YIUIFactory
+    {
+        public static async ETTask<GameObject> InstantiateGameObjectAsync(string resName)
+        {
+            return await InstantiateGameObjectAsync("", resName);
+        }
+    }
+}
