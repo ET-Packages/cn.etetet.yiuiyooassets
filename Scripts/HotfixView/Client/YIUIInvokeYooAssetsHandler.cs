82cd3dd40e5f041a75e3ccf4301458f627899ad6
LiShengYang-yiyi <379338943@qq.com>
2025-06-14 01:11:12 +0800

    ET10

--- Scripts/HotfixView/Client/YIUIInvokeYooAssetsHandler.cs
+++ Scripts/HotfixView/Client/YIUIInvokeYooAssetsHandler.cs
@@ -4,12 +4,12 @@ using YooAsset;
 namespace ET.Client
 {
     [Invoke]
-    public class YIUIInvokeYooAssetsHandler : AInvokeHandler<YIUIInvokeLoadInitialize, ETTask<bool>>
+    public class YIUIInvokeYooAssetsHandler : AInvokeEntityHandler<YIUIInvokeEntity_LoadInitialize, ETTask<bool>>
     {
-        public override async ETTask<bool> Handle(YIUIInvokeLoadInitialize args)
+        public override async ETTask<bool> Handle(Entity entity, YIUIInvokeEntity_LoadInitialize args)
         {
-            YIUILoadComponent loadComponent = args.LoadComponent;
-            if (loadComponent == null)
+            EntityRef<YIUILoadComponent> loadComponentRef = (YIUILoadComponent)entity;
+            if (loadComponentRef.Entity == null)
             {
                 Log.Error($"LoadComponent is null!");
                 return false;
@@ -17,25 +17,25 @@ namespace ET.Client
 
             await ETTask.CompletedTask;
 
-            return loadComponent.AddComponent<YIUIYooAssetsLoadComponent>().Initialize();
+            return loadComponentRef.Entity.AddComponent<YIUIYooAssetsLoadComponent>().Initialize();
         }
     }
 
     [Invoke(EYIUIInvokeType.Sync)]
-    public class YIUIInvokeGetAssetsInfoSyncHandler : AInvokeHandler<YIUIInvokeGetAssetInfo, AssetInfo>
+    public class YIUIInvokeGetAssetsInfoSyncHandler : AInvokeEntityHandler<YIUIInvokeEntity_GetAssetInfo, AssetInfo>
     {
-        public override AssetInfo Handle(YIUIInvokeGetAssetInfo args)
+        public override AssetInfo Handle(Entity entity, YIUIInvokeEntity_GetAssetInfo args)
         {
-            return YIUILoadComponent.Inst?.GetComponent<YIUIYooAssetsLoadComponent>().GetAssetInfo(args.Location, args.AssetType);
+            return entity.YIUILoad()?.GetComponent<YIUIYooAssetsLoadComponent>().GetAssetInfo(args.Location, args.AssetType);
         }
     }
 
     [Invoke(EYIUIInvokeType.Sync)]
-    public class YIUIInvokeGetAssetInfoByGUIDSyncHandler : AInvokeHandler<YIUIInvokeGetAssetInfoByGUID, AssetInfo>
+    public class YIUIInvokeGetAssetInfoByGUIDSyncHandler : AInvokeEntityHandler<YIUIInvokeEntity_GetAssetInfoByGUID, AssetInfo>
     {
-        public override AssetInfo Handle(YIUIInvokeGetAssetInfoByGUID args)
+        public override AssetInfo Handle(Entity entity, YIUIInvokeEntity_GetAssetInfoByGUID args)
         {
-            return YIUILoadComponent.Inst?.GetComponent<YIUIYooAssetsLoadComponent>().GetAssetInfoByGUID(args.AssetGUID, args.AssetType);
+            return entity.YIUILoad()?.GetComponent<YIUIYooAssetsLoadComponent>().GetAssetInfoByGUID(args.AssetGUID, args.AssetType);
         }
     }
 }
